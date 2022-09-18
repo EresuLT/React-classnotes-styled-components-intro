99d108f8a6ed46f7f0bd955eff49947424d1543b
eResult <eresultolu@gmail.com>
2022-09-19 00:25:26 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Button, { TomatoButton } from './components/Button.styled';
+import Container from './components/Container.styled';
+import HeaderText from './components/Header.styled';
+
+const App = () => {
+  return (
+    <>
+      <Container bg="green">
+        <HeaderText>STYLED COMPONENTS</HeaderText>
+        <Button primary>Click</Button>
+        <Button>Click</Button>
+        <TomatoButton>Click</TomatoButton>
+        <TomatoButton primary>Click</TomatoButton>
+        <HeaderText color="white">STYLED COMPONENTS</HeaderText>
+
+
+        <StyledLink href="https://clarusway.com/" target="__blank">
+          CLARUSWAY
+        </StyledLink>
+      </Container>
+    </>
+  );
+};
+
+export default App;
